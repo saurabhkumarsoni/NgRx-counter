ee3873fa296e3c6d97f36129e4427128457f319d
SAURABH KUMAR <saurabh.soni119@gmail.com>
2025-03-27 17:46:16 +0530

    increment decrement and reset counter using input and output

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CounterComponent } from './counter/counter/counter.component';
+import { CounterOutputComponent } from './counter/counter-output/counter-output.component';
+import { CounterButtonComponent } from './counter/counter-button/counter-button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CounterComponent,
+    CounterOutputComponent,
+    CounterButtonComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/counter/counter-button/counter-button.component.ts
@@ -0,0 +1,33 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-counter-button',
+  templateUrl: './counter-button.component.html',
+  styleUrls: ['./counter-button.component.css']
+})
+export class CounterButtonComponent implements OnInit {
+
+@Output() increment = new EventEmitter();
+@Output() decrement = new EventEmitter();
+@Output() reset = new EventEmitter();
+
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onIncrement(){
+this.increment.emit()
+  }
+
+  onDecrement(){
+    this.decrement.emit();
+  }
+
+  onReset(){
+    this.reset.emit()
+  }
+
+}
